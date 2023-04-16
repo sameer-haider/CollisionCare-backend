8bb6899dbe89f4e05fbfb481f07a7bc979836f3d
Yash Pathi <yash.pathi@gmail.com>
2023-04-16 09:12:41 -0500

    Stashing changes

--- app.py
+++ app.py
@@ -5,10 +5,31 @@ import os
 import requests
 import json
 from flask import Flask, jsonify
-
+import mysql.connector
+from models import CollisionReport
+import uuid
+import random
 
 app = Flask(__name__)
 CORS(app)
+
+rand_gen = random.Random()
+
+INSURANCE_ID = 210002
+NAME = "Henry Freud"
+ADDRESS = "2305 West Fairview Lane, Allen, TX, 75093"
+
+app.config["MYSQL_HOST"] = os.environ.get("DATABASE_HOST")
+app.config["MYSQL_USER"] = os.environ.get("DATABASE_USER")
+app.config["MYSQL_PASSWORD"] = os.environ.get("DATABASE_PASSWORD")
+app.config["MYSQL_DB"] = os.environ.get("DATABASE_NAME")
+
+db = mysql.connector.connect(
+    host=os.environ.get("DATABASE_HOST"),
+    user=os.environ.get("DATABASE_USER"),
+    password=os.environ.get("DATABASE_PASSWORD"),
+    database=os.environ.get("DATABASE_NAME"),
+)
 
 
 @app.route("/")
@@ -29,3 +50,51 @@ def process_audio():
     result = {"accident_info": accident_info}
     # return accident info dict
     return jsonify(result), 202
+
+
+#get claim info to display on the geico employee view
+@app.route("/get_report/<int:insurance_id>", methods = ['GET'])
+def get_report(insurance_id):
+    columns = ["report_id", "insurance_id", "type_severity_of_collision", "injuries", "vehicles_involved", "damage_to_customers_car", "location_of_damage", "witnesses", "police_called", "car_is_drivable"]
+    query = f"SELECT * FROM claims_history WHERE insurance_id = {insurance_id}"
+    cur = db.cursor()
+    cur.execute(query)
+    sql_answers = cur.fetchall()
+
+    fin_reports = []
+    for row in sql_answers:
+        reports = {}
+        for i in range(len(columns)):
+            reports[columns[i]] = row[i]
+        fin_reports.append(reports)
+
+    return jsonify(fin_reports), 200
+
+
+
+#Front end needs to send data in the format: 
+        # "type_severity_of_collision",
+        # "injuries",
+        # "vehicles_involved",
+        # "damage_to_customers_car",
+        # "location_of_damage",
+        # "witnesses",
+        # "police_called",
+        # "car_is_drivable"
+
+@app.route("/submit_report", methods = ['POST'])
+def submit_report():
+    data = request.get_json()
+    report_id = generate_uid()
+    report_values = (report_id, INSURANCE_ID, data['type_severity_of_collision'], 
+                             data['injuries'], data['vehicles_involved'], data['damage_to_customers_car'],
+                             data['location_of_damage'], data['witnesses'],
+                             data['police_called'], data['car_is_drivable'])
+    query = "INSERT INTO claims_history (report_id,insurance_id, type_severity_of_collision, injuries, vehicles_involved, damage_to_customers_car, location_of_damage, witnesses, police_called, car_is_drivable) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cur = db.cursor()
+    cur.execute(query, report_values)
+    db.commit()
+    return "Success", 200
+
+def generate_uid():
+    return random.randint(100000, 999999)
