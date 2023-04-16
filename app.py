08d541636086ac7376c351746073a4c742bc61c3
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-15 23:22:13 -0500

    adding app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+import mysql.connector
+
+# import aws_credentials as rds
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    print("hello")
+    return
